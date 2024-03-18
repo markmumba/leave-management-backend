86d779c6d61dcd0f69627f90e3062e9d3a0070de
markmumba <markmumba01@gmail.com>
2024-03-18 07:12:17 +0300

    building the database

--- sql/schema/002_users.sql
+++ sql/schema/002_users.sql
@@ -2,12 +2,15 @@
 CREATE TABLE
   Users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     role_id INT,
     FOREIGN KEY (role_id) REFERENCES Roles(role_id)
   );
+
+
+CREATE INDEX idx_email_on_users ON Users(email);
 
 
 -- +goose Down 
